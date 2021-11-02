32ad690456aa5933c9f21a00a98c0679c14050ef
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-02 14:51:51 +0100

    CSS Vertiefung - Interior

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let inputMail = document.getElementById("inputMail")
+let inputName = document.getElementById("inputName")
+let inputMessage = document.getElementById("inputMessage")
+
+function subscribe(){
+    console.log(inputName.value)
+    console.log(inputMail.value)
+    console.log(inputMessage.value)
+}
